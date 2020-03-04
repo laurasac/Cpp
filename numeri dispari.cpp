2518f6686751978b02ffe8e3322b4c8a36e490b0
Laura Saccani <37377750+laurasac@users.noreply.github.com>
2020-03-04 11:50:00 +0100

    aggiunto programma per numeri dispari

---
+++ numeri dispari.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n = 1 , cont = 1;
+    while(n != 201){
+        if(cont < 5){
+            cout << n << " ";
+            n += 2;
+            cont ++;
+        }else{
+            cout << n << endl;
+            cont = 1;
+            n += 2;
+        }
+    }
+    cin >> n;
+    return 0;
+}
